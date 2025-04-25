845b7ceef4281b97f3a5091525277087bb4da176
AlmostDone123123 <magdalenasroka777@gmail.com>
2025-04-25 10:36:14 +0100

    security issues
    helmet, html sanitize,  mongoinjections

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require("express");
 const path = require("node:path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 const session = require("express-session");
 const flash = require("connect-flash");
 const engine = require("ejs-mate");
@@ -34,18 +36,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 const sessionConfig = {
+  name: "session",
   secret: "thismustbeBetterSecureKey",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(mongoSanitize());
 
+app.use(helmet());
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: false,
+//   })
+// );
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
+];
+const connectSrcUrls = ["https://api.maptiler.com/"];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+        "https://images.unsplash.com/",
+        "https://api.maptiler.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
 //make sure that u use sassion before u se the passport.session as it relies on that
 app.use(passport.initialize());
 app.use(passport.session());
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,7 +3,7 @@ const Review = require("../models/review");
 
 module.exports.createReview = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
-  console.log(campground);
+  // console.log(campground);
   const review = new Review(req.body.review);
   review.author = req.user._id;
   campground.reviews.push(review);
