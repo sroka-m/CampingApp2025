3943af40fbb71bd76133f0b4c489f6f0d2206518
AlmostDone123123 <magdalenasroka777@gmail.com>
2025-05-08 20:29:57 +0100

    fix: favicon added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const path = require("node:path");
+favicon = require("serve-favicon");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -39,6 +40,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 const sessionConfig = {
   name: "session",
   secret: "thismustbeBetterSecureKey",
