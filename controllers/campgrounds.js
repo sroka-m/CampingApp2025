fe4239401e9ee8db9548453a0e063b97d6e58a1d
AlmostDone123123 <magdalenasroka777@gmail.com>
2025-05-21 20:36:31 +0100

    review CRUD on a sperate page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -132,7 +132,10 @@ module.exports.updateCampground = async (req, res) => {
     }
     throw new ExpressError("Total image count cannot exceed 4", 400);
   }
-  await campground.updateOne({ ...req.body.campground });
+  await campground.updateOne(
+    { ...req.body.campground },
+    { runValidators: true }
+  );
   const geoData = await maptilerClient.geocoding.forward(
     req.body.campground.location,
     { limit: 1 }
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,6 +1,42 @@
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
+module.exports.indexReview = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  //i would need to sort them soon
+  //need to calc % for star reivews before i pull out the review made by the user, as it also contributes to the numbers
+  campground.reviews.reverse();
+  // console.log(campground.reviews);
+  //only if the user is logged in, check if he already made a review
+  let userReview;
+  if (res.locals.currentUser) {
+    let index = campground.reviews.findIndex(function (review) {
+      // i could return this or use equal review.author._id.valueOf() == res.locals.currentUser._id.valueOf()
+      return review.author.equals(res.locals.currentUser);
+    });
+    if (index !== -1) {
+      userReview = campground.reviews.splice(index, 1);
+    }
+  }
+
+  const starDesc = ["Terrible", "Not good", "Average", "Very good", "Amazing"];
+
+  res.render("reviews/index", {
+    reviews: campground.reviews,
+    campgroundID: campground._id,
+    title: campground.title,
+    userReview,
+    starDesc,
+  });
+};
+
 module.exports.createReview = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   // console.log(campground);
@@ -10,7 +46,20 @@ module.exports.createReview = async (req, res) => {
   await review.save();
   await campground.save();
   req.flash("success", "Created new review!");
-  res.redirect(`/campgrounds/${campground._id}`);
+  res.redirect(`/campgrounds/${campground._id}/reviews/`);
+};
+
+module.exports.updateReview = async (req, res) => {
+  const { id, reviewId } = req.params;
+
+  const review = await Review.findByIdAndUpdate(
+    reviewId,
+    {
+      ...req.body.review,
+    },
+    { new: true, runValidators: true }
+  );
+  res.redirect(`/campgrounds/${campground._id}/reviews/`);
 };
 
 module.exports.deleteReview = async (req, res) => {
@@ -21,5 +70,5 @@ module.exports.deleteReview = async (req, res) => {
   await Review.findByIdAndDelete(reviewId);
   req.flash("success", "Successfully deleted review!");
 
-  res.redirect(`/campgrounds/${campground._id}`);
+  res.redirect(`/campgrounds/${campground._id}/reviews`);
 };
